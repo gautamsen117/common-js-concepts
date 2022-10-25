954df1e9ef06ad3924c568e9234aee476da78f5f
gautamsen117 <gautamsen117@gmail.com>
2022-10-25 13:24:38 +0600

    hoisting

---
+++ hoisting.js
@@ -0,0 +1,13 @@
+print5();
+// print10();
+for(let i = 0; i < 5; i++){
+    // console.log(i);
+}
+// console.log('outside', i);
+
+function print5(){
+    console.log('inside print5', 5);
+}
+const print10 = function(){
+    console.log('inside print 10', 10);
+}
